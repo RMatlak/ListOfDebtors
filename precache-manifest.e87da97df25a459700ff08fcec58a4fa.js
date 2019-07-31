155b1267c8c8a6117c4bc2126b05535500b72c97
Radoslaw Matlak <radek1999@onet.com.pl>
2019-07-31 16:02:40 +0200

    Updates

---
+++ precache-manifest.e87da97df25a459700ff08fcec58a4fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafefeeee2e5618475ebe48981b81347",
+    "url": "/ListOfDebtors/index.html"
+  },
+  {
+    "revision": "f0971ec892c230e5842c",
+    "url": "/ListOfDebtors/static/css/main.c6ec6792.chunk.css"
+  },
+  {
+    "revision": "f02772bbb4187da3f582",
+    "url": "/ListOfDebtors/static/js/2.1c1333da.chunk.js"
+  },
+  {
+    "revision": "f0971ec892c230e5842c",
+    "url": "/ListOfDebtors/static/js/main.3f54ed7c.chunk.js"
+  },
+  {
+    "revision": "6585225b5a652f0b6af6",
+    "url": "/ListOfDebtors/static/js/runtime~main.de538002.js"
+  }
+]);
